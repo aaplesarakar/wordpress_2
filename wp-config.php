0e61ad0bc0276aac41aefd08cf990efec824915d
aaplesarakar <98375699+aaplesarakar@users.noreply.github.com>
2023-04-06 00:29:27 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'http://digitalsevagov.co.in/' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
